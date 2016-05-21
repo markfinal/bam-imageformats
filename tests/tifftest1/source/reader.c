507b8bf4f7a55d87eba774fee9bd105c6a5ed7cb
Mark Final <markfinal@hotmail.com>
2016-05-21 13:03:14 +0100

    [tests] Added test to exercise writing and reading a TIF

---
+++ tests/tifftest1/source/reader.c
@@ -0,0 +1,78 @@
+/*
+Copyright (c) 2010-2016, Mark Final
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of BuildAMation nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include "reader.h"
+#include "checkerboard.h"
+
+#include "tiffio.h"
+
+#include <string.h>
+
+int
+TestReader()
+{
+    int width = 0;
+    int height = 0;
+    int samplesperpixel = 0;
+    TIFF *tif = TIFFOpen("new.tif", "r");
+    if (NULL == tif)
+    {
+        return -1;
+    }
+    TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);
+    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);
+    TIFFGetField(tif, TIFFTAG_SAMPLESPERPIXEL, &samplesperpixel);
+
+    uint32 *image = _TIFFmalloc(width * height * samplesperpixel);
+    if (NULL == image)
+    {
+        return -1;
+    }
+
+    if (1 != TIFFReadRGBAImage(tif, width, height, image, 0))
+    {
+        return -2;
+    }
+
+    char *compare = _TIFFmalloc(width * height * samplesperpixel);
+    if (NULL == compare)
+    {
+        return -1;
+    }
+    createCheckerboardImage(compare, width, height, samplesperpixel);
+    if (0 != memcmp(image, compare, width * height * samplesperpixel))
+    {
+        return -2;
+    }
+
+    free(compare);
+    free(image);
+    TIFFClose(tif);
+    return 0;
+}
---
+++ tests/tifftest1/source/writer.c
@@ -0,0 +1,88 @@
+/*
+Copyright (c) 2010-2016, Mark Final
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of BuildAMation nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include "writer.h"
+#include "checkerboard.h"
+
+#include "tiffio.h"
+
+#include <stdlib.h>
+
+int
+TestWriter()
+{
+    int width = 128;
+    int height = 128;
+    int samplesperpixel = 4;
+    char *image = malloc(width * height * samplesperpixel);
+    if (NULL == image)
+    {
+        return -1;
+    }
+    size_t linebytes = samplesperpixel * width;
+    unsigned char *buf = NULL;
+    TIFF *tif = TIFFOpen("new.tif", "w");
+    if (NULL == tif)
+    {
+        return -1;
+    }
+    TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, width);
+    TIFFSetField(tif, TIFFTAG_IMAGELENGTH, height);
+    TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, samplesperpixel);
+    TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 8);
+    TIFFSetField(tif, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
+    TIFFSetField(tif, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
+    TIFFSetField(tif, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_RGB);
+    if (TIFFScanlineSize(tif) == linebytes)
+    {
+        buf = _TIFFmalloc(linebytes);
+    }
+    else
+    {
+        buf = _TIFFmalloc(TIFFScanlineSize(tif));
+    }
+    if (NULL == buf)
+    {
+        return -1;
+    }
+    TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, TIFFDefaultStripSize(tif, linebytes));
+    createCheckerboardImage(image, width, height, samplesperpixel);
+    for (unsigned int row = 0; row < height; ++row)
+    {
+        memcpy(buf, &image[(height - row - 1) * linebytes], linebytes);
+        if (TIFFWriteScanline(tif, buf, row, 0) < 0)
+        {
+            break;
+        }
+    }
+    TIFFClose(tif);
+    _TIFFfree(buf);
+    free(image);
+    return 0;
+}
