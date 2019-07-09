73a04f0ee5a5cf319ae939f86a1021e4d391ac4a
Mark Final <markfinal@hotmail.com>
2019-07-09 21:45:54 +0100

    [tests] Now run pngtest1 against both png 1257 and 1626

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -5,9 +5,17 @@ def configure_repository():
     configs["jpegtest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
                                      linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                      osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
-    configs["pngtest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
-                                    linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
-                                    osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs.setdefault("pngtest1", [])
+    configs["pngtest1"].append(TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                        linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                        osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]},
+                                        options="--lpng.version=1257",
+                                        alias="png1.2.x"))
+    configs["pngtest1"].append(TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                         linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                         osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]},
+                                         options="--lpng.version=1626",
+                                         alias="png1.6.x"))
     configs["tifftest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
                                      linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                      osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
