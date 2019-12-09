f42a0387b03ddef5a8402a99e14b0e04a0e6fd17
Mark Final <markfinal@hotmail.com>
2019-12-09 21:09:55 +0000

    [tests] Enable SDK testing

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -1,22 +1,22 @@
-from testconfigurations import TestSetup, visualc, visualc64, visualc32, mingw32, gcc, gcc64, gcc32, clang, clang32, clang64
+from testconfigurations import TestSDKSetup, visualc, visualc64, visualc32, mingw32, gcc, gcc64, gcc32, clang, clang32, clang64
 
 def configure_repository():
     configs = {}
-    configs["jpegtest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
-                                     linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
-                                     osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
-    configs.setdefault("pngtest1", [])
-    configs["pngtest1"].append(TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+    configs["jpegtest1"] = TestSDKSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
                                         linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
-                                        osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]},
-                                        options="--lpng.version=1257",
-                                        alias="png1.2.x"))
-    configs["pngtest1"].append(TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
-                                         linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
-                                         osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]},
-                                         options="--lpng.version=1626",
-                                         alias="png1.6.x"))
-    configs["tifftest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
-                                     linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
-                                     osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+                                        osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs.setdefault("pngtest1", [])
+    configs["pngtest1"].append(TestSDKSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                            linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                            osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]},
+                                            options="--lpng.version=1257",
+                                            alias="png1.2.x"))
+    configs["pngtest1"].append(TestSDKSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                            linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                            osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]},
+                                            options="--lpng.version=1626",
+                                            alias="png1.6.x"))
+    configs["tifftest1"] = TestSDKSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                        linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                        osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     return configs
